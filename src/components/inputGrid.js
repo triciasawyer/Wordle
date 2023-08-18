495782d8541c74658349d2bee984f04fccbff68e
Tricia Sawyer <tricia.sawyer03@gmail>
2023-08-18 10:11:21 -0500

    get 5x5 grid displayed

--- src/components/inputGrid.js
+++ src/components/inputGrid.js
@@ -2,66 +2,70 @@ import React, { useState, useRef } from 'react';
 import { View, TextInput, StyleSheet } from 'react-native';
 
 const InputGrid = ({ size, feedback, onChange, disabled }) => {
-  const [inputs, setInputs] = useState(Array(size).fill(''));
-  const inputRefs = Array.from({ length: size }, () => useRef());
+  const [inputs, setInputs] = useState(Array(size * size).fill(''));
+  const inputRefs = Array.from({ length: size * size }, () => useRef());
 
-  const handleInputChange = (index, value) => {
+  const handleInputChange = (row, col, value) => {
     if (disabled) {
-        return;
+      return;
     }
+    const index = row * size + col;
     const newInputs = [...inputs];
     newInputs[index] = value;
     setInputs(newInputs);
     onChange(newInputs.join(''));
 
-    if (index < size - 1 && value !== '') {
-        inputRefs[index + 1].current.focus();
-      }
+    if (index < size * size - 1 && value !== '') {
+      inputRefs[index + 1].current.focus();
+    }
   };
 
   return (
     <View style={styles.gridContainer}>
-      {inputs.map((input, index) => (
-        <TextInput
-          key={index}
-          style={[
-            styles.gridInput,
-            feedback[index] && feedback[index].correct && styles.correctInput,
-            feedback[index] && !feedback[index].correct && styles.incorrectInput,
-          ]}
-          value={input}
-          onChangeText={(text) => handleInputChange(index, text)}
-          maxLength={1}
-          ref={inputRefs[index]}
-          // Focuses on the first input
-          autoFocus={index === 0}
-          editable={!disabled}
-        />
+      {Array.from({ length: size }).map((_, row) => (
+        <View key={row} style={styles.rowContainer}>
+          {Array.from({ length: size }).map((_, col) => (
+            <TextInput
+              key={col}
+              style={[
+                styles.gridInput,
+                feedback[row * size + col] && feedback[row * size + col].correct && styles.correctInput,
+                feedback[row * size + col] && !feedback[row * size + col].correct && styles.incorrectInput,
+              ]}
+              value={inputs[row * size + col]}
+              onChangeText={(text) => handleInputChange(row, col, text)}
+              maxLength={1}
+              ref={inputRefs[row * size + col]}
+              autoFocus={row === 0 && col === 0}
+              editable={!disabled}
+            />
+          ))}
+        </View>
       ))}
     </View>
-  );
-};
+  )}
 
-const styles = StyleSheet.create({
-  gridContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
-  gridInput: {
-    borderWidth: 1,
-    borderColor: 'gray',
-    width: 40,
-    height: 40,
-    textAlign: 'center',
-    fontSize: 20,
-    margin: 5,
-  },
-  correctInput: {
-    backgroundColor: 'green',
-  },
-  incorrectInput: {
-    backgroundColor: 'yellow',
-  },
-});
+  const styles = StyleSheet.create({
+    gridContainer: {
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      justifyContent: 'center',
+    },
+    gridInput: {
+      borderWidth: 1,
+      borderColor: 'gray',
+      width: 40,
+      height: 40,
+      textAlign: 'center',
+      fontSize: 20,
+      margin: 5,
+    },
+    correctInput: {
+      backgroundColor: 'green',
+    },
+    incorrectInput: {
+      backgroundColor: 'yellow',
+    },
+  });
 
-export default InputGrid;
+  export default InputGrid;
