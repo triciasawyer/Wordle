81eab9482ff64432d28940adede505333e55b741
Tricia Sawyer <tricia.sawyer03@gmail>
2023-08-18 10:23:45 -0500

    fix spelling direction so user types in the first row and then moves to next row

--- src/components/inputGrid.js
+++ src/components/inputGrid.js
@@ -5,18 +5,30 @@ const InputGrid = ({ size, feedback, onChange, disabled }) => {
   const [inputs, setInputs] = useState(Array(size * size).fill(''));
   const inputRefs = Array.from({ length: size * size }, () => useRef());
 
+  const [activeRow, setActiveRow] = useState(0);
+  const [activeCol, setActiveCol] = useState(0);
+
   const handleInputChange = (row, col, value) => {
     if (disabled) {
       return;
     }
-    const index = row * size + col;
-    const newInputs = [...inputs];
-    newInputs[index] = value;
-    setInputs(newInputs);
-    onChange(newInputs.join(''));
+    if (row === activeRow && col === activeCol) {
+      const index = row * size + col;
+      const newInputs = [...inputs];
+      newInputs[index] = value;
+      setInputs(newInputs);
+      onChange(newInputs.join(''));
 
-    if (index < size * size - 1 && value !== '') {
-      inputRefs[index + 1].current.focus();
+      if (col < size - 1 && value !== '') {
+        setActiveCol(col + 1);
+        inputRefs[index + 1].current.focus();
+      } else if (col === size - 1 && value !== '') {
+        setActiveRow(row + 1);
+        setActiveCol(0);
+        if (row < size - 1) {
+          inputRefs[index + size].current.focus();
+        }
+      }
     }
   };
 
@@ -37,35 +49,40 @@ const InputGrid = ({ size, feedback, onChange, disabled }) => {
               maxLength={1}
               ref={inputRefs[row * size + col]}
               autoFocus={row === 0 && col === 0}
-              editable={!disabled}
+              editable={!disabled && row === activeRow}
             />
           ))}
         </View>
       ))}
     </View>
-  )}
+  );
+};
 
-  const styles = StyleSheet.create({
-    gridContainer: {
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      justifyContent: 'center',
-    },
-    gridInput: {
-      borderWidth: 1,
-      borderColor: 'gray',
-      width: 40,
-      height: 40,
-      textAlign: 'center',
-      fontSize: 20,
-      margin: 5,
-    },
-    correctInput: {
-      backgroundColor: 'green',
-    },
-    incorrectInput: {
-      backgroundColor: 'yellow',
-    },
-  });
+const styles = StyleSheet.create({
+  gridContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  gridInput: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    width: 40,
+    height: 40,
+    textAlign: 'center',
+    fontSize: 20,
+    margin: 5,
+  },
+  correctInput: {
+    backgroundColor: 'green',
+  },
+  incorrectInput: {
+    backgroundColor: 'yellow',
+  },
+});
 
-  export default InputGrid;
+export default InputGrid;
